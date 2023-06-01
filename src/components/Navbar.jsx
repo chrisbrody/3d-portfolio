681c383e4ff67e9d74377aa22c24d368c5a8fee9
chrisbrody <chrisbrody@codifyacademy.com>
2023-06-01 11:40:31 -0400

    fix: updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Adrian &nbsp;
-            <span className='sm:block hidden'> | JavaScript Mastery</span>
+            Brody &nbsp;
+            <span className='sm:block hidden'> | Building Dreams, One Project at a Time. </span>
           </p>
         </Link>
 
